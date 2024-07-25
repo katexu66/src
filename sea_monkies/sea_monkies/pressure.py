146df35b9f68d675a21faaad61e3f9f5e2a03071
katexu66 <katexu66@gmail.com>
2024-07-25 11:09:46 -0400

    Making launch file a .yaml instead of .py

--- sea_monkies/sea_monkies/pressure.py
+++ sea_monkies/sea_monkies/pressure.py
@@ -8,7 +8,6 @@ from sensor_msgs.msg import (
     Temperature,
 )
 
-
 class pressure_node(Node):
 
     def __init__(self):
@@ -19,19 +18,33 @@ class pressure_node(Node):
         self.sub = self.create_subscription(
             Pressure,
             "bluerov2/pressure",
-            self.callback,
+            self.pressure_callback,
             10
+        )
+
+        self.depth_pub = self.create_publisher(
+            Altitude,
+            "bluerov2/depth",
+            # self.depth_callback,
+            10 #figure out how to publish to this topic
         )
 
         self.get_logger().info("starting subscriber nodes")
 
-    def callback(self, msg):
+    def pressure_callback(self, msg):
         self.get_logger().info(f"Pressure: {msg.fluid_pressure}")
         self.pressure = msg.fluid_pressure
+        
+    def depth_calculation(self):
+        # p = p*g*h
+        gravity = 9.81 # m/s2
+        density = 1000 # kg/m3
+        self.depth = self.pressure/(gravity*density)
+        msg = self.depth
+        self.publisher.publish(msg)
 
     def destroy_node(self):
         return super().destroy_node()
-
 
 def main(args=None):
     rclpy.init(args=args)
