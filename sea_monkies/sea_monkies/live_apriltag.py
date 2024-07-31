613a4952b401c19582848f42cba9704788f063c2
katexu66 <katexu66@gmail.com>
2024-07-31 17:55:05 -0400

    Rn it works

--- sea_monkies/sea_monkies/live_apriltag.py
+++ sea_monkies/sea_monkies/live_apriltag.py
@@ -16,6 +16,13 @@ class ImageSubscriber(Node):
         self.angle = 0
         self.desired_heading = 0
         self.center = 320
+        self.at_detector = Detector(families='tag36h11',
+                    nthreads=1,
+                    quad_decimate=1.0,
+                    quad_sigma=0.0,
+                    refine_edges=1,
+                    decode_sharpening=0.25,
+                    debug=0)
 
         super().__init__("image_subscriber")
 
@@ -60,53 +67,50 @@ class ImageSubscriber(Node):
         img = cv2.imread("image.png", cv2.IMREAD_GRAYSCALE)
         
         camera_params = (1061, 1061, 2000/2, 1121/2) #fx, fy, cx, cy
-        at_detector = Detector(families='tag36h11',
-                    nthreads=1,
-                    quad_decimate=1.0,
-                    quad_sigma=0.0,
-                    refine_edges=1,
-                    decode_sharpening=0.25,
-                    debug=0)
-        tags = at_detector.detect(img, estimate_tag_pose=True, camera_params=camera_params, tag_size=0.1)
+
+        tags = self.at_detector.detect(img, estimate_tag_pose=True, camera_params=camera_params, tag_size=0.1)
         color_img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+        
+        if len(tags) > 0:
+            for tag in tags:
+                for idx in range(len(tag.corners)):
+                    cv2.line(color_img, tuple(tag.corners[idx - 1, :].astype(int)), tuple(tag.corners[idx, :].astype(int)), (0, 255, 0))
 
-        for tag in tags:
-            for idx in range(len(tag.corners)):
-                cv2.line(color_img, tuple(tag.corners[idx - 1, :].astype(int)), tuple(tag.corners[idx, :].astype(int)), (0, 255, 0))
+                cv2.putText(color_img, str(tag.tag_id),
+                            org=(tag.corners[0, 0].astype(int) + 10, tag.corners[0, 1].astype(int) + 10),
+                            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                            fontScale=0.8,
+                            color=(0, 0, 255))
+                
+                translation = tag.pose_t #translation vector/position of tag in camera frame (x,y,z axes)
+                distance = tag.pose_t[2]/2.8 #z-axis for distance
+                #rotation = tag.pose_R
+                tag_id = tag.tag_id
+                corners = tag.corners #x,y coordinates of 4 corners detected
+                self.center = tag.center #x,y coordinates of center of tag detected
 
-            cv2.putText(color_img, str(tag.tag_id),
-                        org=(tag.corners[0, 0].astype(int) + 10, tag.corners[0, 1].astype(int) + 10),
-                        fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                        fontScale=0.8,
-                        color=(0, 0, 255))
+                if self.center[0] > 400:
+                    self.desired_heading = self.angle + self.center[0]/400*90
+                elif self.center[0] < 240:
+                    self.desired_heading = self.angle - self.center[0]/400*90
+                else:
+                    self.desired_heading = self.angle
+
+                msg = Int16()
+                msg.data = self.desired_heading
+                self.publisher.publish(msg)
             
-            translation = tag.pose_t #translation vector/position of tag in camera frame (x,y,z axes)
-            distance = tag.pose_t[2]/2.8 #z-axis for distance
-            #rotation = tag.pose_R
-            tag_id = tag.tag_id
-            corners = tag.corners #x,y coordinates of 4 corners detected
-            self.center = tag.center #x,y coordinates of center of tag detected
+                self.get_logger().info(f"Tag ID: {tag_id}")
+                self.get_logger().info(f"Corners: {corners}")
+                self.get_logger().info(f"Center: {self.center}")
+                self.get_logger().info(f"Translation: {translation}")
+                self.get_logger().info(f"Distance: {distance}")
+                #self.get_logger().info(f"Rotation: {rotation}")
+                self.get_logger().info("---")
 
-            if self.center[0] > 400:
-                self.desired_heading = self.angle + 5
-            elif self.center[0] < 240:
-                self.desired_heading = self.angle - 5
-            else:
-                self.desired_heading = self.angle
+            cv2.imwrite("detected.png", color_img)
 
-            msg = Int16()
-            msg.data = self.desired_heading
-            self.publisher.publish(msg)
-            
-            self.get_logger().info(f"Tag ID: {tag_id}")
-            self.get_logger().info(f"Corners: {corners}")
-            self.get_logger().info(f"Center: {self.center}")
-            self.get_logger().info(f"Translation: {translation}")
-            self.get_logger().info(f"Distance: {distance}")
-            #self.get_logger().info(f"Rotation: {rotation}")
-            self.get_logger().info("---")
 
-        cv2.imwrite("detected.png", color_img)
 
 def main(args=None):
     rclpy.init(args=args)
